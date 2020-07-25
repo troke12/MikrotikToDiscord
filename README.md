e3850877192e4e0e5b31fab6b2897889b8030a37
troke12 <ochi@troke.id>
2020-07-25 09:03:23 +0800

    :rocket: Initial Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Simple Webhook
+
+This script is for my mikrotik netwatch and log into my discord channel, if my devices are down from mikrotik they will automatic posted to my discord channel
+
+## Usage
+
+- Use `form.php?text=YOURTEXT&id=FIRSTIDWEBHOOK&token=TOKENWEBHOOK`
+
+![gambar](https://cdn.discordapp.com/attachments/408950289962369025/736387195509932152/unknown.png)
+
+## Results
+
+![gambar](https://cdn.discordapp.com/attachments/408950289962369025/736386992035725332/unknown.png)
---
+++ form.php
@@ -0,0 +1,39 @@
+<?php
+if (isset($_GET['text']) AND isset($_GET['id']) AND isset($_GET['token']))
+{
+   $text=$_GET['text'];
+   $id=$_GET['id'];
+   $token=$_GET['token'];
+   $webhookurl= "https://discordapp.com/api/webhooks/$id/$token";
+   $json_data = json_encode([
+    // Message
+    "content" => "$text"
+   ], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE );
+
+   $ch = curl_init( $webhookurl );
+   curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+   curl_setopt( $ch, CURLOPT_POST, 1);
+   curl_setopt( $ch, CURLOPT_POSTFIELDS, $json_data);
+   curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
+   curl_setopt( $ch, CURLOPT_HEADER, 0);
+   curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
+
+   $response = curl_exec( $ch );
+   curl_close( $ch );
+
+}
+
+else
+{
+   header("Location:form.php?error=variabel_belum_diset");
+}
+
+if(empty($text))
+{
+   header("Location:form.php?error=text_kosong");
+}
+else
+{
+   echo "Sukses";
+}
+?>
